8ff7254ff8644b19dae308396eba8b138c01096a
spamakin <60445095+Spamakin@users.noreply.github.com>
2024-01-03 23:03:44 -0600

    minor changes

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -47,6 +47,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+(straight-use-package 'org)
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
