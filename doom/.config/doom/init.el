7173c2689cb3a4e186342a7caefc204ed854e3b3
spamakin <60445095+Spamakin@users.noreply.github.com>
2024-12-13 17:29:29 -0600

    Might as well use LSP

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -146,7 +146,8 @@
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
       (latex            ; writing papers in Emacs has never been so fun
-       +latexmk)          ; what else would you use?
+       +latexmk         ; what else would you use?
+       +lsp)
        ;;+cdlatex          ; quick maths symbols
        ;;+fold             ; fold the clutter away nicities
        ;;lean              ; for folks with too much to prove
